4c85065cfd9335284fde5a5825c1902c92903a15
AdiL18 <adi.l@comstar.co.il>
2020-11-30 15:14:52 +0200

    project init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./Header";
+import Title from "./Title";
+
+class App {
+    render() {
+        return (
+            <Header>
+                <Title text="Hello World" />
+            </Header>
+        );
+    }
+}
+
+export default App;
